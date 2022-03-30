3ebc7f5828145b3b9ba2927f83b81173b48db966
ykunisato <ykunisato@psy.senshu-u.ac.jp>
2022-03-31 04:32:48 +0900

    update

--- template-jsPsych7.0/name_of_repository/task.js
+++ template-jsPsych7.0/name_of_repository/task.js
@@ -1,8 +1,8 @@
 /* 課題に関するコードを以下に書く */
-var welcome = {
+const welcome = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: "こんにちは！ この課題は何かキーボードのキーをタイプするとデータを保存して終わります。"
 };
 
 /*タイムラインの設定*/
-var timeline = [fullscreen,welcome];
+const timeline = [fullscreen,welcome];
